6559db2cc314ee9e5a9e4409fb785108bfb9c75a
Fikret9 <37271703+Fikret9@users.noreply.github.com>
2024-07-30 20:00:28 -0500

    Update server.js again

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const publicPath = path.join(__dirname,'..','public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));;
+app.use(xyz);
 
 app.get('*',(req,res)=>{ // unhandled requests
   res.sendFile(path.join(publicPath,'index.html'));

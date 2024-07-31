6ea18798e6332c459d65c47d79b34ff50a573a2a
ftokyay <ftokyay@miu.edu>
2024-07-30 19:53:39 -0500

    changed server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname,'..','public');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+app.use(express.static(publicPath));;
 
 app.get('*',(req,res)=>{ // unhandled requests
   res.sendFile(path.join(publicPath,'index.html'));
